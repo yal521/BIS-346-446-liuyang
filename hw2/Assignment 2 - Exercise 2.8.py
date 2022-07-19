6ff86cc90591d52563ac7bed125ec694e8fed386
yal521 <yal521@lehigh.edu>
2022-07-19 09:11:40 -0400

    update 2.8-2

--- hw2/Assignment 2 - Exercise 2.8.py
+++ hw2/Assignment 2 - Exercise 2.8.py
@@ -1,6 +1,6 @@
 print(' number square cube')
 for x in range(0, 6):
-  print('{:< 7d}{:< 7d}{:< 7d}'.format(x, x*x, x*x*x))# -*- coding: utf-8 -*-
+  print('{:< 7d}{:< 7d}{:< 7d}'.format(x, x*x, x*x*x))
 print(' number square   cube')
 for x in range(0, 6):
   print('{:> 7d}{:> 7d}{:> 7d}'.format(x, x*x, x*x*x))
