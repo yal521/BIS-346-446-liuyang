b1df04d63ef285475a9cda97d0670e019fbee7b9
yal521 <yal521@lehigh.edu>
2022-07-18 23:13:02 -0400

    update 3.1-1

--- hw2/Assignment2-Exercise3.1.py
+++ hw2/Assignment2-Exercise3.1.py
@@ -1,21 +1,23 @@
-# Exercise 3.1
+# initialize variables
 passes = 0  # number of passes
+failures = 0  # number of failures
 
-# process 10 students
+# process 10 students   
 for student in range(10):
     # get one exam result
-    result = int(input('Enter result (1=pass, 2=fail): '))
-    
-    while result != 1 and result != 2:  
-        print('Incorrect result entered.')
-        result = int(input('Enter result (1=pass, 2=fail): '))
-        
+    result = 0
+    while result not in [1, 2]:
+       print('Incorrect result entered.')
+       result = int(input('Enter result (1=pass, 2=fail): '))
+
     if result == 1:
-        passes = passes + 1
+       passes = passes + 1
+    else:
+       failures = failures + 1
 
 # termination phase
 print('Passed:', passes)
-print('Failed:', 10 - passes)
+print('Failed:', failures)
 
 if passes > 8:
     print('Bonus to instructor')
