b8a8fe3e7c1e3df0f2a10fe5fc742053d1d61a78
yal521 <yal521@lehigh.edu>
2022-07-26 08:08:21 -0400

    6.5

--- hw3/Assignment3_exercise6.5.py
+++ hw3/Assignment3_exercise6.5.py
@@ -1,5 +1,5 @@
 text = ('This is sample text with several words '
-'this is more sample text with some different words')
+        'this is more sample text with some different words')
 word_counts = {}
 textcap = text.upper() #ignore upper and lower case
 # count occurrences of each unique word
@@ -9,15 +9,13 @@ for word in textcap.split():
     else:
         word_counts[word] = 1
 
-print(f'{"WORD":<18} COUNT')
+print(f'{"WORD":<29} COUNT')
 
 for word, count in sorted(word_counts.items()):
-    print(f'{word:<20}{count}')
+    print(f'{word:<30}{count}')
 
-    print('\nNumber of unique words: ',len(word_counts) )
-
-
-    print("Duplicate words: ")
+print('--------------------------------------')    
+print("Duplicate Words")  
 for word, count in word_counts.items():
     if word_counts[word] > 1:
-        print(f'{word:<20}')
+        print(f'{word:<30}')
