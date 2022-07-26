0087f397cf9a1f402fed15f2d76c2a36f48eca7c
yal521 <yal521@lehigh.edu>
2022-07-26 05:27:33 -0400

    5.20

--- hw3/Assignment3_5.20.py
+++ hw3/Assignment3_5.20.py
@@ -1,13 +1,13 @@
 def display_table(lst):
     print("\t",end="")
-    for i in range(len(lst[0])):
-        print(i+1,end="\t")
+    for a in range(len(lst[0])):
+        print(a+1,end="\t")
     print()
 
-    for i in range(len(lst)):
-        print(i+1,end="\t")
-        for col in lst[i]:
+    for a in range(len(lst)):
+        print(a+1,end="\t")
+        for col in lst[a]:
             print(col,end="\t")
         print()
 # Testing
-display_table([[23,62,93,31],[72,14,575,61],[2,6,13,5]])
+display_table([[34,98,23,35],[62,34,255,91],[3,4,9,5]])
